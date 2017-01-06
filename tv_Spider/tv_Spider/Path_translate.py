0ee904685a6c0d80e5f962c5d4170c48254f98db
tingyun <13284638588@163.com>
2017-01-06 15:22:43 +0800

    update tudou_tv,douban_tv crawler , do some specical for douban_tv.

--- tv_Spider/tv_Spider/Path_translate.py
+++ tv_Spider/tv_Spider/Path_translate.py
@@ -63,13 +63,11 @@ def Get_Valid_Url(urls):
 			
 
 def get_HeadUrl(index_url,spider_name):
-	if spider_name == "youku_show":
+	if spider_name == "youku_tv":
 			return re.sub(r"\d.html","{page}.html",index_url)
-	if spider_name == "mangguo_show":
+	if spider_name == "mangguo_tv":
 			return re.sub(r"\d-0--.html","{page}-0--.html",index_url)
-	if spider_name == "mangguo_sp":
-			return re.sub(r"\d-0--.html","{page}-0--.html",index_url)
-	if spider_name == "souhu_show":
+	if spider_name == "souhu_tv":
 			return re.sub(r"\d_p11_p12_p13.html","{page}_p11_p12_p13.html",index_url)
 	else:
 			return re.sub("(\d+)$","{page}",index_url)
--- tv_Spider/tv_Spider/Total_page_circulate.py
+++ tv_Spider/tv_Spider/Total_page_circulate.py
@@ -1,22 +1,23 @@
 #!coding=utf-8
 
-#这个文件：分离出所有需要计算站点实际page页面的木块，一个可扩展的if elif语句来实现
+#这个文件：分离出所有需要计算站点实际page页面的模块，一个可扩展的if elif语句来实现
 
+#这个是对所有的第一级页面的总的page数的一个统计
 def Total_page_circulate(site_name,max_pages):
-	if site_name == "letv_tv":
-			return (max_pages/30)+1
+	if site_name == "tudou_tv":
+			return (max_pages/90)+1
 	else:
 			return max_pages
 
 
 def Turn_True_Page(i,site_name):
-	if site_name == "tencent_show":
-			return (i - 1)*30
+	if site_name == "douban_tv":
+			return (i - 1)*20
 	else:
 			return i
 
 
-
+#这个在除了第一级页面，之后可能会分多页面的次级的页面（例如虾米专辑：第一级分了很多艺人名字的页面，然后进入到某个艺人的专辑页面，由于专辑数比较多，又分成了多个同级页面）
 def Total_page_circulate2(site_name,max_pages):
 	if site_name == "tudou_show":
 			return (max_pages/90)+1
